e18c5071e037f5208ad3c16332fa120e0e81cddf
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-12 10:27:07 +0200

    levenshtein is benne

--- node.js
+++ node.js
@@ -1,2 +1,13 @@
 cica = require('./x.js');
 console.log(cica.szoroz(2, 3));
+var Q = require('spque').Queue
+var Levenshtein = require('levenshtein')
+let q = new Q()
+
+q.put(2)
+q.put(4)
+var t = new Levenshtein( 'jézuska', 'géppuska' ).distance
+console.log(
+    q.size, q.get(), q.get(), t
+)
+//2 2
